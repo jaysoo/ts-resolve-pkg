1668d117e2fc1c3c44053dce8af519862a8ae743
Jack Hsu <jack.hsu@gmail.com>
2023-02-02 12:43:01 -0500

    Update README.md

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-Try `node index.js` and see that it fails the first time, but subsequence calls work.
+This is a reproduction for a bug where `ensurePackage` does not work with `typescript`. Even after `typescript` is installed, Node still cannot resolve it unless you re-run the script.
 
-Also note that if you remove `node_modules` it also works. It seems to be a problem when `node_modules` exist but typescript is not found.
+## Steps
 
-If you try a different package it also works: `node index.js date-fns`.
+1. Try `node index.js` and see that it fails the first time.
+2. Call `node index.js` a second time and it works.
+
+> Note: if you remove `node_modules` first then the script works. It seems to be a problem when `node_modules` exist but typescript is not found.
+
+## Notes
+
+- `moment` also fails.
+- If you try a different package it also works: `node index.js date-fns`.
+- If you remove the `require.resolve(pkg)` call then the script works the first time as well.
--- index.js
+++ index.js
@@ -1,29 +1,56 @@
-// Put this file somewhere, and run `npm init`.
-// Then `node index.js` and see that it will error the first time.
-// If you run it a second time it works.
-//
-// Also if you use another package like date-fns,  it works the frist time.
-const child_process_1 = require("child_process");
-const pkg = process.argv[2] ?? 'typescript';
+const { execSync } = require("child_process");
+const { existsSync } = require("fs");
+const { join } = require("path");
+const semver = require("semver");
+const pkg = process.argv[2] ?? "typescript";
+const ver = process.argv[3];
 
+async function initGenerator() {
+  const detectedVersion = getPackageVersion(pkg);
+  console.log(">>> found", detectedVersion);
 
-function ensurePackage(tree, pkg, requiredVersion, options = {}) {
+  if (!detectedVersion) {
+    console.log(">> installing...");
+    execSync(`npm i -D ${pkg}`, {
+      stdio: [0, 1, 2],
+    });
+  } else if (ver && !semver.satisfies(detectedVersion, ver)) {
+    console.log(">> installing...");
+    execSync(`npm i -D ${pkg}`, {
+      stdio: [0, 1, 2],
+    });
+  }
+
+  require.resolve(pkg);
+}
+
+function detectPackageManager(dir = process.cwd()) {
+  return existsSync(join(dir, "yarn.lock"))
+    ? "yarn"
+    : existsSync(join(dir, "pnpm-lock.yaml"))
+      ? "pnpm"
+      : "npm";
+}
+
+function getPackageVersion(pkg) {
   try {
-    require.resolve(pkg);
+    const pm = detectPackageManager();
+    if (pm === "pnpm") {
+      const json = JSON.parse(execSync(`pnpm list --depth=0 --json ${pkg}`));
+      return json.dependencies[pkg]?.version ?? null;
+    } else if (pm === "yarn") {
+      const json = JSON.parse(
+        execSync(
+          `yarn list --pattern ${pkg} --depth=0 --json --non-interactive --no-progress`
+        )
+      );
+      return json?.type?.trees?.find((t) => t.name === pkg)?.version;
+    } else {
+      const json = JSON.parse(execSync(`npm list --depth=0 --json ${pkg}`));
+      return json.dependencies[pkg]?.version ?? null;
+    }
+  } catch {
+    return null;
   }
-  catch (_d) {
-  }
-  (0, child_process_1.execSync)(`npm i -D ${pkg}@${requiredVersion}`, {
-    cwd: process.cwd(),
-    stdio: [0, 1, 2],
-  });
-  return
 }
-
-
-function initGenerator(host, schema) {
-  ensurePackage(host, pkg, 'latest');
-  console.log(`> resolved at ${require.resolve(pkg)}`);
-}
-
 initGenerator();
